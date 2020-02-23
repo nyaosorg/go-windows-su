c221c79ca6f6162c3b97549921bfeb6d3c211232
HAYAMA_Kaoru <iyahaya@nifty.com>
2020-02-24 00:36:08 +0900

    sucopy/pack.cmd: makes package for sucopy

---
+++ cmd/sucopy/pack.cmd
@@ -0,0 +1,12 @@
+setlocal
+set /P "VERSION=version ?"
+for /F %%I in ('cd') do set NAME=%%~nI
+for %%I in (386 amd64) do call :build %%I
+endlocal
+exit /b 0
+
+:build
+    set GOARCH=%1
+    go build -ldflags "-s -w"
+    zip -9 %NAME%-windows-%1-%VERSION%.zip %NAME%.exe
+    exit /b
