bb89de7aab9179210e09ae3bcf49272ac8e4872a
HAYAMA_Kaoru <iyahaya@nifty.com>
2020-02-23 23:51:18 +0900

    sucopy: recover netdrive on administrator mode

--- cmd/sucopy/main.go
+++ cmd/sucopy/main.go
@@ -6,27 +6,35 @@ import (
 	"os"
 	"strings"
 
+	"github.com/zetamatta/go-windows-netresource"
 	"github.com/zetamatta/go-windows-su"
 )
 
 func mains(args []string) error {
 	var buffer strings.Builder
-
 	buffer.WriteString(`/s /c "`)
-	dir, err := os.Getwd()
-	if err != nil {
-		return err
+
+	if dir, err := os.Getwd(); err == nil {
+		fmt.Fprintf(&buffer, `cd "%s" & `, dir)
 	}
-	fmt.Fprintf(&buffer, `cd "%s" && copy`, dir)
+
+	if netDrives, err := netresource.GetNetDrives(); err == nil {
+		for _, n := range netDrives {
+			fmt.Fprintf(&buffer, `net use %c: "%s" 2>nul & `,
+				n.Letter, n.Remote)
+		}
+	}
+
+	buffer.WriteString("copy ")
 	for _, s := range args {
-		fmt.Fprintf(&buffer, ` "%s"`, s)
+		fmt.Fprintf(&buffer, `"%s" `, s)
 	}
-	buffer.WriteString(` & pause"`)
+	buffer.WriteString(`& pause"`)
 
 	param := buffer.String()
 
-	fmt.Println(param)
-	_, err = su.ShellExecute(su.RUNAS, "cmd.exe", param, "")
+	fmt.Printf("cmd %s\n", param)
+	_, err := su.ShellExecute(su.RUNAS, "cmd.exe", param, "")
 	return err
 }
 
