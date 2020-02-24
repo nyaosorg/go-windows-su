7c3172860cdcea2bb7e2cb12a1eef9f4e81ed0d3
HAYAMA_Kaoru <iyahaya@nifty.com>
2020-02-24 12:27:02 +0900

    cmd/sucopy/pack.cmd: use upx

--- cmd/sucopy/pack.cmd
+++ cmd/sucopy/pack.cmd
@@ -1,5 +1,5 @@
 setlocal
-set /P "VERSION=version ?"
+set /P "VERSION=version ? "
 for /F %%I in ('cd') do set NAME=%%~nI
 for %%I in (386 amd64) do call :build %%I
 endlocal
@@ -8,5 +8,6 @@ exit /b 0
 :build
     set GOARCH=%1
     go build -ldflags "-s -w"
+    upx -9 %NAME%.exe
     zip -9 %NAME%-windows-%1-%VERSION%.zip %NAME%.exe
     exit /b
